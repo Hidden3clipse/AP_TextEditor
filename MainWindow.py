491e3624375bf6015651ebe038518b48f5dde493
chey00 <115455604+chey00@users.noreply.github.com>
2024-12-11 13:42:21 +0100

    Added slots and new task

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import pyqtSlot
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QMainWindow, QMenu, QMenuBar, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QMenu, QMenuBar, QFileDialog, QFontDialog
 
 from CentralWidget import CentralWidget
 
@@ -9,6 +9,9 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
+        self.__initial_filter = "Default files (*.txt)"
+        self.__filter = self.__initial_filter + ";;All files (*)"
+
         central_widget = CentralWidget(self)
 
         self.setWindowTitle("Mein Texteditor")
@@ -16,13 +19,23 @@ class MainWindow(QMainWindow):
         menuBar = QMenuBar(self)
 
         files = QMenu("Files", menuBar)
+
         action_file_open = files.addAction("Open ...")
         action_file_open.triggered.connect(self.file_open)
+
         action_file_save = files.addAction("Save ...")
+        action_file_save.triggered.connect(self.file_save)
+
         action_file_copy = files.addAction("Copy ...")
+        action_file_copy.triggered.connect(self.file_copy)
+
         action_file_move = files.addAction("Move ...")
+        action_file_move.triggered.connect(self.file_move)
 
         menuBar.addMenu(files)
+
+        action_font = menuBar.addAction("Font")
+        action_font.triggered.connect(self.font)
 
         self.setMenuBar(menuBar)
 
@@ -30,10 +43,27 @@ class MainWindow(QMainWindow):
 
     @pyqtSlot()
     def file_open(self):
-        (path, filter) = QFileDialog.getOpenFileName(self, "Open File", "./", "Text files (*.txt);;All files (*)")
-
-        print(path)
-        print(filter)
+        (path, self.__initial_filter) = QFileDialog.getOpenFileName(self, "Open File", "./", self.__filter, self.__initial_filter)
 
         if path:
             pass
+            # Ersetzen Sie die jeweilige alte Statusmeldung durch den ausgewählten Pfad
+
+    @pyqtSlot()
+    def file_save(self):
+        (path, self.__initial_filter) = QFileDialog.getSaveFileName(self, "Save File", "./", self.__filter, self.__initial_filter)
+
+        if path:
+            pass
+
+    @pyqtSlot()
+    def file_copy(self):
+        pass
+
+    @pyqtSlot()
+    def file_move(self):
+        pass
+
+    @pyqtSlot()
+    def font(self):
+        QFontDialog.show()
