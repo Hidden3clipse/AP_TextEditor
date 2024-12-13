6d035c6dd287f22e3a3312e4758b49c0ac39a563
chey00 <115455604+chey00@users.noreply.github.com>
2024-12-13 09:04:23 +0100

    Implemented text handler and layout to Central Widget

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,6 +1,30 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtWidgets import QWidget, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton
 
 
 class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
+
+        bar_layout = QHBoxLayout()
+
+        pushbutton_bold = QPushButton('Bold')
+        pushbutton_italic = QPushButton('Italic')
+        pushbutton_under = QPushButton('Underline')
+
+        bar_layout.addWidget(pushbutton_bold)
+        bar_layout.addWidget(pushbutton_italic)
+        bar_layout.addWidget(pushbutton_under)
+
+        self.__text_edit = QTextEdit()
+
+        layout = QVBoxLayout()
+
+        layout.addLayout(bar_layout)
+        layout.addWidget(self.__text_edit)
+
+        self.setLayout(layout)
+
+    @pyqtSlot(str)
+    def set_text(self, text):
+        self.__text_edit.setText(text)
